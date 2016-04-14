d30de1fee6e839395355afb54e9c69428233517b
Kevin Risden <risdenk@avalonconsult.com>
2016-04-14 16:45:08 -0500

    Fix R scripts

--- r-base/scripts/entrypoint.sh
+++ r-base/scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export CLASSPATH="$(echo $(ls /opt/solr/dist/solr-solrj* /opt/solr/dist/solrj-lib/*) | tr ' ' ':')"
-
 for SCRIPT in $(ls /opt/scripts/*.R)
 do
   echo $SCRIPT
