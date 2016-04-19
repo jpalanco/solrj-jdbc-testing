6bc960d3f50759966646d4a0b05c432362f7c108
Kevin Risden <risdenk@avalonconsult.com>
2016-04-19 10:22:19 -0500

    Added entrypoint for DbVisualizer to fix Solr driver on startup

---
+++ dbvisualizer/scripts/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+LOCATIONS=$(ls /opt/solr/dist/solrj-lib/* | awk '{print "<Location type=\"dynamic\"><Path>" $0 "</Path></Location>"}' | sed -e 's/\//\\\//g' | sed -e 's/"/\\"/g' | tr -d '\n')
+DRIVER_LOCATION=$(ls /opt/solr/dist/solr-solrj-*.jar | awk '{print "<Location type=\"dynamic\"><Path>" $0 "</Path><Classes><Class type=\"jdbc\">org.apache.solr.client.solrj.io.sql.DriverImpl</Class></Classes></Location>"}' | sed -e 's/\//\\\//g' | sed -e 's/"/\\"/g' | tr -d '\n')
+
+sed -i'' -e "s/LOCATION_REPLACE/$LOCATIONS/" /root/.dbvis/config70/dbvis.xml
+sed -i'' -e "s/DRIVER_REPLACE/$DRIVER_LOCATION/" /root/.dbvis/config70/dbvis.xml
+
+/usr/local/bin/dbvis
