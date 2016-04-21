b6e33253e2ab10a6d072cd911f0357401be45c84
Kevin Risden <risdenk@avalonconsult.com>
2016-04-21 14:20:25 -0500

    Switch dbvisualizer to alpine

--- dbvisualizer/scripts/entrypoint.sh
+++ dbvisualizer/scripts/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 LOCATIONS=$(ls /opt/solr/dist/solrj-lib/* | awk '{print "<Location type=\"dynamic\"><Path>" $0 "</Path></Location>"}' | sed -e 's/\//\\\//g' | sed -e 's/"/\\"/g' | tr -d '\n')
 DRIVER_LOCATION=$(ls /opt/solr/dist/solr-solrj-*.jar | awk '{print "<Location type=\"dynamic\"><Path>" $0 "</Path><Classes><Class type=\"jdbc\">org.apache.solr.client.solrj.io.sql.DriverImpl</Class></Classes></Location>"}' | sed -e 's/\//\\\//g' | sed -e 's/"/\\"/g' | tr -d '\n')
