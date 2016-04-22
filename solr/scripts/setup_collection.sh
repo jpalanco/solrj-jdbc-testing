de78416093b8ea786235ac0c8077961f0643e977
Kevin Risden <risdenk@avalonconsult.com>
2016-04-22 11:24:11 -0500

    Fix Solr configs

--- solr/scripts/setup_collection.sh
+++
@@ -1,7 +0,0 @@
-OUTPUT=/opt/docker-solr/status.log
-echo "starting $0; logging to $OUTPUT"
-{
-    /opt/docker-solr/scripts/wait-for-solr.sh
-    /opt/solr/bin/solr create -c test -d /opt/sa_conf/
-    /opt/solr/bin/post -c test /opt/sa_data/
-} </dev/null >$OUTPUT 2>&1 &
