40f55bd0e3ab2fc95e2ab207f75b410f112e10bb
Kevin Risden <risdenk@avalonconsult.com>
2016-04-22 08:52:26 -0500

    Move Spark to alpine

--- spark/scripts/entrypoint.sh
+++ spark/scripts/entrypoint.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 export SOLR_CP="$(echo $(ls /opt/solr/dist/solr-solrj* /opt/solr/dist/solrj-lib/*) | tr ' ' ':')"
 
-for SCRIPT in $(ls /opt/scripts/*.scala)
+for SCRIPT in $(ls /scripts/*.scala)
 do
   echo $SCRIPT
-  /opt/spark/bin/spark-shell --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') -i $SCRIPT
+  /spark/bin/spark-shell --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') -i $SCRIPT
 done
 
-for SCRIPT in $(ls /opt/scripts/*.py /opt/scripts/*.R)
+for SCRIPT in $(ls /scripts/*.py /scripts/*.R)
 do
   echo $SCRIPT
-  /opt/spark/bin/spark-submit --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') $SCRIPT
+  /spark/bin/spark-submit --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') $SCRIPT
 done
 
-for SCRIPT in $(ls /opt/scripts/*.sql)
+for SCRIPT in $(ls /scripts/*.sql)
 do
   echo $SCRIPT
-  /opt/spark/bin/spark-sql --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') -i $SCRIPT
+  /spark/bin/spark-sql --master local[*] --verbose --driver-class-path $SOLR_CP --jars $(echo $SOLR_CP | tr ':' ',') -i $SCRIPT
 done
 
