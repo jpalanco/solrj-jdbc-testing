7ab711923d0526356cf22ece5d958367f6197752
Kevin Risden <risdenk@avalonconsult.com>
2016-09-29 08:07:54 -0500

    Make X11 running more secure

--- run_x11.sh
+++ run_x11.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 open -a xquartz
-xhost +
-export X11_DISPLAY=$(ipconfig getifaddr en0):0
+HOST_IP=$(ipconfig getifaddr en0)
+xhost +$HOST_IP
+export X11_DISPLAY=$HOST_IP:0
 
 for x in dbvisualizer squirrelsql;
 do
