a49759e147105d4843912ca2b5e17eb03de80180
Kevin Risden <risdenk@avalonconsult.com>
2016-09-29 09:14:33 -0500

    Remove commented out code

--- r-base/scripts/test_solr.R
+++ r-base/scripts/test_solr.R
@@ -10,10 +10,5 @@ drv <- JDBC("org.apache.solr.client.solrj.io.sql.DriverImpl",
 conn <- dbConnect(drv, "jdbc:solr://solr:9983?collection=test", "user", "pwd")
 dbGetQuery(conn, "select fielda, fieldb, fieldc, fieldd_s, fielde_i from test limit 10")
 
-#dbListTables(conn)
-#data(iris)
-#dbWriteTable(conn, "iris", iris, overwrite=TRUE)
-#dbGetQuery(conn, "select count(*) from iris")
-#d <- dbReadTable(conn, "iris")
-
 dbDisconnect(conn)
+
