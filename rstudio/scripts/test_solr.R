aa1f3a579f053dc1e8285a980b4906bc8fc93297
Kevin Risden <risdenk@avalonconsult.com>
2016-09-30 14:27:05 -0500

    Make RStudio smaller

--- rstudio/scripts/test_solr.R
+++ rstudio/scripts/test_solr.R
@@ -1,6 +1,7 @@
 # https://www.rforge.net/RJDBC/
 
-install.packages("RJDBC", dep=TRUE)
+# Already installed
+# install.packages("RJDBC", dep=TRUE)
 
 library("RJDBC")
 
@@ -12,10 +13,5 @@ drv <- JDBC("org.apache.solr.client.solrj.io.sql.DriverImpl",
 conn <- dbConnect(drv, "jdbc:solr://solr:9983?collection=test", "user", "pwd")
 dbGetQuery(conn, "select fielda, fieldb, fieldc, fieldd_s, fielde_i from test limit 10")
 
-#dbListTables(conn)
-#data(iris)
-#dbWriteTable(conn, "iris", iris, overwrite=TRUE)
-#dbGetQuery(conn, "select count(*) from iris")
-#d <- dbReadTable(conn, "iris")
-
 dbDisconnect(conn)
+
