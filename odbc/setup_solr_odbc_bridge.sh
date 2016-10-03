7037bead1b876061cec3c7c5346a0bbe5ff04e89
Kevin Risden <risdenk@avalonconsult.com>
2016-10-03 16:43:59 -0500

    Add latest Solr build for odbc setup like rest of solrj-jdbc-testing repo

--- odbc/setup_solr_odbc_bridge.sh
+++ odbc/setup_solr_odbc_bridge.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-#wget -O- http://apache.mirrors.lucidnetworks.net/lucene/solr/6.2.1/solr-6.2.1.tgz
-
 set -u
 set -e
 
-SOLR_VERSION=6.2.1
+SOLR_BASE_VERSION=7.0.0
+SOLR_BUILD_NUMBER=$(wget -qO- https://builds.apache.org/job/Solr-Artifacts-master/lastSuccessfulBuild/buildNumber)
+SOLR_VERSION="${SOLR_BASE_VERSION}-${SOLR_BUILD_NUMBER}"
+SOLR_URL="https://builds.apache.org/job/Solr-Artifacts-master/lastSuccessfulBuild/artifact/solr/package/solr-${SOLR_VERSION}.tgz"
 
 OUTPUT_DIR=/Library/Java/Extensions
 
 sudo rm -rf $OUTPUT_DIR/*
-cat solr-${SOLR_VERSION}.tgz | sudo tar zx -C $OUTPUT_DIR/ - "solr-${SOLR_VERSION}/dist/solr-solrj-*.jar" "solr-${SOLR_VERSION}/dist/solrj-lib/*"
+wget -qO- $SOLR_URL | sudo tar zx -C $OUTPUT_DIR/ - "solr-${SOLR_VERSION}/dist/solr-solrj-*.jar" "solr-${SOLR_VERSION}/dist/solrj-lib/*"
 sudo mv $OUTPUT_DIR/solr-${SOLR_VERSION}/dist/*.jar $OUTPUT_DIR/solr-${SOLR_VERSION}/dist/solrj-lib/* $OUTPUT_DIR/
 sudo rm -rf $OUTPUT_DIR/solr-${SOLR_VERSION} $OUTPUT_DIR/guava*.jar
 
