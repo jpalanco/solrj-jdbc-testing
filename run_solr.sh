77fe7f60eec02a80eab5c85b175675ad912ec7f6
Kevin Risden <risdenk@avalonconsult.com>
2016-10-03 16:29:05 -0500

    Added http and zk port output info

--- run_solr.sh
+++ run_solr.sh
@@ -3,5 +3,12 @@
 echo "Running Apache Solr"
 docker-compose up -d solr;
 sleep 10
-open "http://localhost:$(docker-compose port solr 8983 | cut -d':' -f2)"
+
+PORT_8983=$(docker-compose port solr 8983 | cut -d':' -f2 2>/dev/null)
+PORT_9983=$(docker-compose port solr 9983 | cut -d':' -f2 2>/dev/null)
+
+echo "HTTP port: $PORT_8983"
+echo "ZK port: $PORT_9983"
+
+open "http://localhost:${PORT_8983}"
 
