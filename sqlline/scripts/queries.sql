dfd8962b96cc8584ad06c16d9dc9f5d2e08e20ab
Kevin Risden <risdenk@avalonconsult.com>
2016-10-04 14:32:25 -0500

    Add more queries for sqlline

--- sqlline/scripts/queries.sql
+++ sqlline/scripts/queries.sql
@@ -1,3 +1,7 @@
+-- Not yet supported
+select * from test limit 10;
+SELECT COUNT(*) FROM (select COUNT(*) from test limit 100) t;
+-- supported
 select COUNT(*) from test;
 select fielda from test limit 10;
 select fielda, fieldb, fieldc from test limit 10;
@@ -7,3 +11,4 @@ select fielda, min(fieldc) as min from test group by fielda;
 select fielda, fieldb, fieldc from test order by fielda desc;
 select fielda, fieldb, fieldc, fieldd_s, fielde_i from test limit 10;
 select fielda, fieldb, min(fieldc), max(fieldc), avg(fieldc), sum(fieldc) from test group by fielda, fieldb;
+
